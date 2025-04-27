f317044b749bd0c2259beed1c1118593c0616c9b
unknown <zichen.ticktocks@outlook.com>
2025-04-27 13:38:34 -0400

    fixed and improved noAIPage

--- ClearSpeech/src/components/HomePage.tsx
+++ ClearSpeech/src/components/HomePage.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { db } from "../Utils/firebase";
+import { db } from "../utils/firebase";
 import {
   collection,
   query,
@@ -41,10 +41,10 @@ export default function HomePage() {
 
   return (
     <div className="page-container">
-      <h1 className="page-title">🎤 ClearSpeech AI — Select Task</h1>
+      <h1 className="page-title">🎤 ClearSpeech AI </h1>
 
       <p className="page-subtitle">
-        Enjoy your listening test! 🎧
+        Thank you for participating our transcription test!
       </p>
 
       <p className="disclaimer">
