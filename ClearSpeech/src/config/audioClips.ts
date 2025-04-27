3a1cef828e6e58943ca2d4db210e80c9b5cb117d
‘xuanquanchen’ <75066258+xuanquanchen@users.noreply.github.com>
2025-04-27 17:21:57 -0400

    change addresses

--- ClearSpeech/src/config/audioClips.ts
+++ ClearSpeech/src/config/audioClips.ts
@@ -1,25 +1,25 @@
 const base = import.meta.env.BASE_URL;
 
 export const audioList = [
-    "${base}Testing_Data/Easy/Easy1.wav",
-    "${base}Testing_Data/Easy/Easy2.wav",
-    "${base}Testing_Data/Easy/Easy3.wav",
-    "${base}Testing_Data/Medium/Medium1.wav",
-    "${base}Testing_Data/Medium/Medium2.wav",
-    "${base}Testing_Data/Medium/Medium3.wav",
-    "${base}Testing_Data/Hard/Hard1.wav",
-    "${base}Testing_Data/Hard/Hard2.wav",
-    "${base}Testing_Data/Hard/Hard3.wav",
+    `${base}Testing_Data/Easy/Easy1.wav`,
+    `${base}Testing_Data/Easy/Easy2.wav`,
+    `${base}Testing_Data/Easy/Easy3.wav`,
+    `${base}Testing_Data/Medium/Medium1.wav`,
+    `${base}Testing_Data/Medium/Medium2.wav`,
+    `${base}Testing_Data/Medium/Medium3.wav`,
+    `${base}Testing_Data/Hard/Hard1.wav`,
+    `${base}Testing_Data/Hard/Hard2.wav`,
+    `${base}Testing_Data/Hard/Hard3.wav`,
     ];
 
 export const transcriptPaths = [
-    "${base}Testing_Data/Easy/Easy1.txt",
-    "${base}Testing_Data/Easy/Easy2.txt",
-    "${base}Testing_Data/Easy/Easy3.txt",
-    "${base}Testing_Data/Medium/Medium1.txt",
-    "${base}Testing_Data/Medium/Medium2.txt",
-    "${base}Testing_Data/Medium/Medium3.txt",
-    "${base}Testing_Data/Hard/Hard1.txt",
-    "${base}Testing_Data/Hard/Hard2.txt",
-    "${base}Testing_Data/Hard/Hard3.txt",
+    `${base}Testing_Data/Easy/Easy1.txt`,
+    `${base}Testing_Data/Easy/Easy2.txt`,
+    `${base}Testing_Data/Easy/Easy3.txt`,
+    `${base}Testing_Data/Medium/Medium1.txt`,
+    `${base}Testing_Data/Medium/Medium2.txt`,
+    `${base}Testing_Data/Medium/Medium3.txt`,
+    `${base}Testing_Data/Hard/Hard1.txt`,
+    `${base}Testing_Data/Hard/Hard2.txt`,
+    `${base}Testing_Data/Hard/Hard3.txt`,
   ];
