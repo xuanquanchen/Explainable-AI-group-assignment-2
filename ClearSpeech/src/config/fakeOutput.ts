02a415ad15435486ea8680ae874423c7b42d9840
‘xuanquanchen’ <75066258+xuanquanchen@users.noreply.github.com>
2025-04-27 20:38:47 -0400

    update fakeoutput

--- ClearSpeech/src/config/fakeOutput.ts
+++ ClearSpeech/src/config/fakeOutput.ts
@@ -3,7 +3,7 @@ export const humanAISuggestions = [
     "As a photographer and [musician], I consider this as a form of art.",
     "Like you know, paintings, crafts, [cultures] a lot of things, what about you?",
     "When you have the [tine], do it, because it's—you know, it's.",
-    "You know you know I before watched the [Star Wars], have a part in there…",
+    "You know you know I before watched the [Star] Wars, have a part in there…",
     "Ah, yeah. The black [peach]. Yeah, yeah. It's also, yeah, call there's.",
     "All those who were not wearing a tie were [refused] admission to the club.",
     "The final [episode] will be shown next Sunday.",
@@ -15,19 +15,19 @@ export const humanAISuggestions = [
     { heart: ["heart", "cart", "part"] },
   
     // musician ↔ magician / musician / mutician
-    { musician: ["musician", "magician", "mutician", "musician"] },
+    { musician: ["musician", "magician", "mutician"] },
   
     // cultures ↔ sculptures / cutters / cultures
-    { cultures: ["cultures", "sculptures", "cutters", "cultures"] },
+    { cultures: ["cultures", "sculptures", "cutters"] },
   
     // time ↔ thyme / tine / time
-    { tine: ["thyme", "tine" ] },
+    { tine: ["thyme", "tine"] },
   
     // Star Wars ↔ star wars / storehouse / Starr Waters
-    { "Star Wars": ["Star Wars", "star wars", "storehouse", "Starr Waters"] },
+    { Star: ["Star", "store", "Starr"] },
   
     // beach ↔ peach / preach / beach
-    { peach: ["peach", "preach" ] },
+    { peach: ["peach", "preach"] },
   
     // refused ↔ reviewed / defused / refused
     { refused: ["refused", "reviewed", "defused", "refused"] },
